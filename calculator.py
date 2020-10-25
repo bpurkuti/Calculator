a47aa01d24cf33425f593266bc4970403d6a8f57
Bishwo Purkuti <bishwopurkutihhs@gmail.com>
2020-10-24 21:44:20 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,28 +1,22 @@
-
 from tkinter import*
+import os
 
 root = Tk()
 
-def addBtn():
-    myLabel = Label(root, text="Click! Click!")
-    myLabel.grid(row=5,column=5)
-a=b=c=0
-btnlist = []
-for i in range (1,10):
-    if(i<4):
-        myBtn = Button(root, text=i, command=addBtn).grid(row=0, column=a)
-        btnlist.append(myBtn)
-        a+=1
-    elif(i<7):
-        myBtn = Button(root, text=i).grid(row=1, column=b)
-        btnlist.append(myBtn)
-        b+=1
-    else:
-        myBtn = Button(root, text=i).grid(row=2, column=c)
-        btnlist.append(myBtn)
-        c+=1
+e = Entry(root,width=50,borderwidth=10, fg="white", bg="green")
+e.pack()
+e.insert(0,"Enter Something here")
 
-#btnlist[1].grid(row=5, column=5)
+def addBtn():
+   hello = "Hello " + e.get()
+   myLabel = Label(root, text=hello)
+   myLabel.pack()
+
+
+
+
+myBtn = Button(root, text="Hello", command=addBtn)
+myBtn.pack()
 
 root.mainloop()
 
